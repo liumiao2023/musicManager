c3aa0b4b9762e11940ff0b3dba10ef94e34ff433
liumiao2023 <liumiao2023@hotmail.com>
2016-10-21 11:54:28 +0800

    create item index

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161021031432) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.string   "length"
+    t.string   "url"
+    t.float    "usd"
+    t.float    "rmb"
+    t.integer  "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
